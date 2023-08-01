d7cab376d70ef2124ea3bd97518e5efb00d363f8
unknown <rafal.paciorek19@gmail.com>
2023-08-01 13:32:22 +0200

    displayed first commit

--- TaskManager/authSystem/views.py
+++ TaskManager/authSystem/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# czy hasła sa takie same
+# czy username wolny
+# czy email wolny
+# walidacja hasła
+
+
 def register(request):
 
     return render(request, 'register.html')
